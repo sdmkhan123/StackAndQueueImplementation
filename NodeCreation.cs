ecf7246e85da20de455bb9ad3be87dc0a5dcc08c
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-22 01:50:28 +0530

    pop element from the stack one by one

--- NodeCreation.cs
+++ NodeCreation.cs
@@ -1,5 +1,4 @@
 ﻿using StackAndQueueImplementation.StackDetailImplementaion;
-using StackAndQueueImplementation.QueueDetailImplementation;
 using System;
 using System.Collections.Generic;
 using System.Text;
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using StackAndQueueImplementation.StackDetailImplementaion;
-using StackAndQueueImplementation.QueueDetailImplementation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -16,22 +15,16 @@ namespace StackAndQueueImplementation
             while (true)
             {
                 Console.WriteLine("****************************************************************");
-                Console.WriteLine("Press 1 : for stack \nPress 2 : for Queue\nPress 0 : to exit");
+                Console.WriteLine("Press 1 : for stack \nPress 0 : to exit");
                 Console.WriteLine("****************************************************************\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         Console.WriteLine("****************************************************************");
-                        Console.WriteLine("Welcome to  Queue");
+                        Console.WriteLine("Welcome to  Stack");
                         Console.WriteLine("****************************************************************\n");
                         Stack.stackMehtod();
-                        break;
-                    case 2:
-                        Console.WriteLine("****************************************************************");
-                        Console.WriteLine("Welcome to  Queue");
-                        Console.WriteLine("****************************************************************\n");
-                        Queue.queueMehtod();
                         break;
                     case 0:
                         Environment.Exit(0);

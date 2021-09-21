5bdf41be736dd5fe2b70c66330c44e07e73281fd
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-21 20:38:29 +0530

    Welcome to  Stack and Queue

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StackAndQueueImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("Welcome to  Stack and Queue");
+            Console.WriteLine("****************************************************************");
+        }
+    }
+}
